8bc2ce01dc3d54689c5b7ccbbd2cc02d97fc67b3
SylvSylv <SylvSylv@users.noreply.github.com>
2019-08-22 16:01:40 +0200

    Organizing test in categories

--- farkle_tests.ts
+++ farkle_tests.ts
@@ -10,42 +10,16 @@ beforeEach(() => {
     farkle = new Farkle();   // #1
 })
 
-describe('Farkle score', () => {
+describe('Basic score', () => {
     it('should return 100 for a 1', () => {
         expect(farkle.score([1])).to.equal(100)
     })
     it('should return 50 for a 5', () => {
         expect(farkle.score([5])).to.equal(50)
-    }) 
-    it('should return 1000 for a 1,1,1', () => {
-        expect(farkle.score([1,1,1])).to.equal(1000)
-    }) 
-    it('should return 200 for a 2,2,2', () => {
-        expect(farkle.score([2,2,2])).to.equal(200)
     })
+})
 
-
-    it('should return 300 for a 3,3,3', () => {
-        expect(farkle.score([3,3,3])).to.equal(300)
-    })
-    it('should return 400 for a 4,4,4', () => {
-        expect(farkle.score([4,4,4])).to.equal(400)
-    })
-    it('should return 500 for a 5,5,5', () => {
-        expect(farkle.score([5,5,5])).to.equal(500)
-    })
-    it('should return 600 for a 6,6,6', () => {
-        expect(farkle.score([6,6,6])).to.equal(600)
-    })
-    
-    it('should return 400 for a 4,4,4,1', () => {
-        expect(farkle.score([4,4,4,1])).to.equal(400)
-    })
-
-    it('should return 2000 for a 1,1,1,1', () => {
-        expect(farkle.score([1,1,1,1])).to.equal(2000)
-    })
-
+describe("Using a roll object", () => {
     it("CountDieTypes: should count 3 die1's for 1,1,1", () => {
         const result: any = farkle.countDieTypes([1,1,1]);
         expect(result).to.deep.equal({die1: 3});
@@ -67,6 +41,38 @@ describe('Farkle score', () => {
             die6: 2,
         });
     })
+})
+
+describe("Roll with triplet", () => {
+    it('should return 1000 for a 1,1,1', () => {
+        expect(farkle.score([1,1,1])).to.equal(1000)
+    }) 
+    it('should return 200 for a 2,2,2', () => {
+        expect(farkle.score([2,2,2])).to.equal(200)
+    })
+    it('should return 300 for a 3,3,3', () => {
+        expect(farkle.score([3,3,3])).to.equal(300)
+    })
+    it('should return 400 for a 4,4,4', () => {
+        expect(farkle.score([4,4,4])).to.equal(400)
+    })
+    it('should return 500 for a 5,5,5', () => {
+        expect(farkle.score([5,5,5])).to.equal(500)
+    })
+    it('should return 600 for a 6,6,6', () => {
+        expect(farkle.score([6,6,6])).to.equal(600)
+    })
+
+    it('should handle extra die and return 400 for a 4,4,4,1', () => {
+        expect(farkle.score([4,4,4,1])).to.equal(400)
+    })
+})
+
+describe("Roll with quatern", () => {
+    it('should return 2000 for a 1,1,1,1', () => {
+        expect(farkle.score([1,1,1,1])).to.equal(2000)
+    })
+
 })
 
 
